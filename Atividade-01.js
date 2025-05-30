29480d06bee164621550de7f0f7086f301b4d1de
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 10:58:21 -0300

    Adiciona novos elementos ao array

---
+++ Atividade-01.js
@@ -0,0 +1,14 @@
+// 1 – Cadastro de novos produtos
+let promocoes = [
+    "teclado mecânico",
+    "mouse gamer",
+    "monitor ultrawide",
+    "cadeira ergonômica",
+    "webcam HD",
+    "fone bluetooth",
+    "notebook i5",
+    "hub USB"
+  ];
+  
+promocoes.push (`SSD 1TB`, `Mesa digializadora`);
+console.log(`Cadastros de novos produtos atualizados: ${promocoes}`);
