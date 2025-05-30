827c8a670e042427c15859c2fe4502880471fe45
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:31:10 -0300

    Verifica elemento do array

---
+++ Atividade-10.js
@@ -0,0 +1,10 @@
+// 10 – Verificação de produto em estoque
+let estoque = [
+    "mesa para PC",
+    "cadeira gamer",
+    "monitor 27 polegadas",
+    "hub USB",
+    "mouse sem fio"
+  ];
+
+  console.log(`A presenca inclui cadeira gamer? ${estoque.includes("cadeira gamer")}`);
