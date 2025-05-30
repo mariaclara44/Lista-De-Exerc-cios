f7ac64999c0ed0457baac7a4df8e57bc3a928d2f
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:37:24 -0300

    Coverte array para um string

---
+++ Atividade-11.js
@@ -0,0 +1,12 @@
+// 11 – Geração de frase
+let ingredientes = [
+    "farinha de trigo",
+    "fermento",
+    "leite",
+    "ovos",
+    "açúcar",
+    "manteiga"
+  ];
+  
+  console.log(`Ingredientes como string: ${ingredientes.join(', ')}`);
+  
