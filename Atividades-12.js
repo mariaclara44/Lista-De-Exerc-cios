51005af920ed3aeef13d6f7b6665628f20583ea9
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:41:06 -0300

    Coverte array para um string

---
+++ Atividades-12.js
@@ -0,0 +1,11 @@
+// 12 – Tags para URL
+
+let palavrasChave = [
+    "desenvolvimento",
+    "frontend",
+    "javascript",
+    "css",
+    "react"
+  ];
+  
+  console.log(`Palavras-Chaves como string: ${palavrasChave.join('- ')}`);
