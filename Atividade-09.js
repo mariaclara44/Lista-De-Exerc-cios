e818994945da4183e0b0f5b9a9d7ab21c73b7fb8
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:30:10 -0300

    Verifica elemento do array

---
+++ Atividade-09.js
@@ -0,0 +1,10 @@
+// 9 – Conferência de presença
+let presenca = [
+    "João Lima",
+    "Carla Nunes",
+    "Amanda Souza",
+    "Bruno Ribeiro",
+    "Fernanda Sales"
+  ];
+  
+console.log(`A presenca inclui Amanda Souza? ${presenca.includes("Amanda Souza")}`);
