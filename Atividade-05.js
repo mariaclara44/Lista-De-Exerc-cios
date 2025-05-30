b10e3eab477f6f28b3649a0eeaffc5ed1432018f
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:16:06 -0300

    Remove o primeiro elemento do array

---
+++ Atividade-05.js
@@ -0,0 +1,12 @@
+// 5 – Erro de importação
+let importados = [
+    "carregador portátil",
+    "cabo HDMI",
+    "leitor biométrico",
+    "controle sem fio",
+    "webcam 4K"
+  ];
+
+let elementoRemovidoComDefeito =  importados.shift();
+console.log(`A lista de produtos importada atualizada é: ${importados}`);
+console.log(`O produto removido foi: ${elementoRemovidoComDefeito}`);
