fc1c835840f2512d3b7dbbef04c3e61400bcf290
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:26:54 -0300

    Adiciona dois elementos no ínicio do array

---
+++ Atividade-08.js
@@ -0,0 +1,13 @@
+// 8 – Alunos transferidos
+let chamada = [
+    "Carlos",
+    "Fernanda",
+    "João",
+    "Mariana",
+    "Lucas",
+    "Camila"
+  ];
+  chamada.unshift('Beatriz', `Rafaela` );
+  console.log(`Chamada atualizada: ${chamada}`);
+  console.table(chamada);
+  
