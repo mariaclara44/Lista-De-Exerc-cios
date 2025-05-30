2a6072fa0637dbb4a98f7bc543e29c1dbe3646bc
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:56:24 -0300

    Filtra os elementos do array

---
+++ Atividades-13.js
@@ -0,0 +1,16 @@
+// 13 – Relatório de férias: Primeira semana
+
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+  ];
+
+  console.log(`Relatório: ${diasFaltas.splice(0, 5)} `);
+
+  
