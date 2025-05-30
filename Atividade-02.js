698b544176f52cf64dec3ff6546f1756dace746a
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:04:03 -0300

    Insira novos elementos ao array

---
+++ Atividade-02.js
@@ -0,0 +1,14 @@
+//  2 – Expansão de cardápio
+let cardapio = [
+    "pizza de calabresa",
+    "lasanha",
+    "sushi",
+    "salada Caesar",
+    "pastel",
+    "yakisoba",
+    "feijoada"
+  ];
+
+  cardapio.push (`Hambúrguer artesanal`, `Warp de frango`);
+  console.log(`Cardapio atualizado: ${cardapio}`);
+  
