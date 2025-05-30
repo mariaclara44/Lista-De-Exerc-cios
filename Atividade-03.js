18d5edb09075d654d8fa1978ed013fe94b6e43ed
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-05-30 11:07:36 -0300

    Insira novos elementos ao array

---
+++ Atividade-03.js
@@ -0,0 +1,15 @@
+//  3 – Último produto com defeito
+let producao = [
+    "mousepad",
+    "placa de vídeo",
+    "fonte de alimentação",
+    "HD externo",
+    "notebook gamer",
+    "roteador"
+  ];
+  
+let elementoRemovido = producao.pop();
+console.log(`Após o pop: ${producao}`);
+console.log(`Elemneto removido: ${elementoRemovido}`);
+
+
