fb5f5c8659aabf532532fe76f17ed14a666fb855
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-06-09 09:10:11 -0300

    Feat: Inverte a ordem da lista

---
+++ Atividades-25.js
@@ -0,0 +1,15 @@
+let atualizacoes = [
+  "Backup diário concluído",
+  "Servidor reiniciado",
+  "Nova versão implantada",
+  "Logs exportados",
+  "Firewall atualizado",
+  "Monitoramento ativado",
+  "Banco de dados otimizado",
+  "Script de segurança executado"
+];
+
+
+let atualizacoesInvertida = atualizacoes.reverse();
+
+console.log("A lista de atualizações invertida:", atualizacoesInvertida);
