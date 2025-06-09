a2a0443f5fa9ae731d22ffe9c75869f339d62060
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-06-09 09:07:46 -0300

    Feat: Ordene a lista de vendas

---
+++ Atividades-24.js
@@ -0,0 +1,13 @@
+let vendas = [3500,
+     1800,
+      4200, 
+      3900, 
+      5000, 
+      1200, 
+      2900, 
+      4700];
+
+
+let listasDeVendasOrdenadas = vendas.sort();
+
+console.log("A lista de vendas ordenados:", listasDeVendasOrdenadas);
