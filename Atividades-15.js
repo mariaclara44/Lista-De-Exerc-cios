d510fe0839b641a94c004badae33373897b92501
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-06-09 08:12:14 -0300

    Feat:Remove os elementos na posição 2 e 3

---
+++ Atividades-15.js
@@ -0,0 +1,11 @@
+let menuNavegacao = [
+  "Início",
+  "Serviços",
+  "Portfólio",
+  "Blog",
+  "Contato",
+  "Login",
+  "Área do cliente"
+];
+
+console.log(`Os 4 artigos mais recentes: ${menuNavegacao.splice(1, 2)} `);
