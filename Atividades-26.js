94661e3003bcc30ff2cd4097c03fceeffc5a3a5e
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-06-09 09:13:31 -0300

    Feat: Inverte a ordem da lista de historico

---
+++ Atividades-26.js
@@ -0,0 +1,13 @@
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+
+let listaDeHistoricoInvertida = historico.reverse();
+
+console.log("A lista de historico invertida:", listaDeHistoricoInvertida);
