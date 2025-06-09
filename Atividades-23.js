ffc5603afa2360ba7bf7411ff62dbf7a372004ef
Maria Clara <maria.c.ferreira47@aluno.senai.br>
2025-06-09 09:02:27 -0300

    Feat: Ordene a lista de candidatos

---
+++ Atividades-23.js
@@ -0,0 +1,15 @@
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+
+
+let listaDeCandidatosOrdenados = candidatos.sort();
+
+console.log("A lista de Candidatos ordenados:", listaDeCandidatosOrdenados);
